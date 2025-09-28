a4664706a19465b52d0ce740d5ca7a911b9676fe
pawank <pawan.kadapatti@tarento.com>
2025-09-29 01:29:11 +0530

    add Storage service

---
+++ lib/services/secure_storage_service.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  SecureStorageService._();
+  static final SecureStorageService instance = SecureStorageService._();
+
+  final FlutterSecureStorage storage = const FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+    iOptions: IOSOptions(
+        accessibility: KeychainAccessibility.first_unlock_this_device),
+  );
+}
