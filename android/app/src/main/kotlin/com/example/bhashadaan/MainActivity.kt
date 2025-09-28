b59bfc71f18c12a3914695d0e90a748b8bcbfe40
pawank <pawan.kadapatti@tarento.com>
2025-09-29 00:56:31 +0530

    updated namespace

--- android/app/src/main/kotlin/com/example/bhashadaan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bhashadaan/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.bhashadaan
+package gov.bhashini.bhashadaan
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
