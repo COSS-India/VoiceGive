181106d10d620a3a586d8e0fcf8a15ddfb84c4e1
Shankar Ganesh <shankar.ganesh@tarento.com>
2025-09-30 11:53:04 +0530

    changed file names and structure

--- lib/screens/bolo_india/bolo_contribute/bolo_contribute.dart
+++ lib/screens/bolo_india/bolo_contribute/bolo_contribute.dart
@@ -1,9 +1,9 @@
 import 'package:bhashadaan/common_widgets/custom_app_bar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:bhashadaan/screens/bolo_screen/widgets/actions_section.dart';
-import 'package:bhashadaan/screens/bolo_screen/widgets/bolo_content_section.dart';
-import 'package:bhashadaan/screens/bolo_screen/widgets/bolo_headers_section.dart';
-import 'package:bhashadaan/screens/bolo_screen/widgets/language_selection.dart';
+import 'package:bhashadaan/screens/bolo_india/widgets/actions_section.dart';
+import 'package:bhashadaan/screens/bolo_india/widgets/bolo_content_section.dart';
+import 'package:bhashadaan/screens/bolo_india/widgets/bolo_headers_section.dart';
+import 'package:bhashadaan/screens/bolo_india/widgets/language_selection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
